97ee44c595c1b6a9d7a9c73d65d8b1761d3293da
srkngzdmr <162897145+srkngzdmr@users.noreply.github.com>
2026-03-02 01:32:01 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -755,4 +755,5 @@ def rapor_endpoint():
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=False)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
